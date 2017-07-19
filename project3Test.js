00110d8fb765260379251697791619d5c6db9d77
monikak96 <monikakrishna96@gmail.com>
2017-07-19 17:33:50 +0530

    Update project3Test.js

--- project3Test.js
+++ project3Test.js
@@ -1,23 +1,28 @@
 var http=require("http");
-var Code = require("../g");
+var Code = require("../proj3Mocha");
 var fs= require("fs");
 describe("reset function",function(){
 	it("reading file",function(){
 		fs.readFile('./filenew.js','utf8',function(err,data){
 			if(err){ throw 'error occured';}
-			var Obj = Code.reset.Obj;
-	
-	it("writing file",function()
-				{
+		var Obj = JSON.parse(data);
+		if(Obj[Code.value1]==undefined){
+				var items = Code.itemsAvailable;
+				Obj[Code.value1]= {"model":"nokia","count":0,"userid":1234,"itemsbought":[items[0].available]}; 
+				var w = JSON.stringify(Obj);
 					fs.writeFile('./filenew.js',Code.reset.w,'utf8',function(err,data){
-						if(err)throw 'error writing file';})
-				})
+			if(err)throw 'error writing file';});
+		}
 				
-			
-		});
+				
+				
+				})
+	});
+		
+		
 			
 	});
-});
+
 
 describe('/',function(){
 		before(function (done) {
@@ -37,15 +42,18 @@ http.get("http://localhost:3000",function(res)
 			assert.equal(Code.server.res,'u have chosen nokia\n');
 			fs.readFile('./filenew.js','utf8',function(err,data){
 				if(err) throw 'error reading file';
-			})
-		}
-	})
-	it('checking count value',function(){
-		
-		if(Code.server.Obj[Code.value1].count==0){
+				else var Obj = JSON.parse(data);
+				
+				if(Obj[Code.value1].count==0){
+			
 		assert.equal(Code.server.res,'u can buy the item')
-		fs.writeFile('./filenew.js',Code.server.t,'utf8',function(err,data){
+		var t = Code.Obj[Code.value];
+		
+		fs.writeFile('./filenew.js',t,'utf8',function(err,data){
 			if(err) throw ' cannot write file'
+		})
+		}
+	
 		})
 		}
 	})
